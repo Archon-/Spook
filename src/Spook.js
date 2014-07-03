dba0a50c1cda7ae6b30d79e2c1858b5e8b8c3cfb
Archon- <archon@archons.info>
2014-07-04 00:53:59 +0200

    JSDoc and initialization

---
+++ src/Spook.js
@@ -0,0 +1,11 @@
+/**
+ *	Spook - JavaScript Spirite Canvas Game Framework
+ *	@author Archon Shigeru
+ *	@license MIT
+ *	@version 0.0.1
+ */
+var Spook = function(CanvasWidth, CanvasHeight, CanvasId){
+	return rtue;
+}
+
+window['spook'] = spook;
